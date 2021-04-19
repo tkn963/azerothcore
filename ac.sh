3c4bc17542fd599abb6cecb0579d39ebe7faff15
Revision <tkn963@gmail.com>
2021-04-19 21:29:14 +0200

    Initial setup
    Created the ifs for accepted functions.
    
    Added a basic function to print when invalid arguments are used. It will be changed to show a list of valid arguments soon.

--- ac.sh
+++ ac.sh
@@ -9,3 +9,25 @@ for i in "${INCLUDES[@]}"; do
         exit 1
     fi
 done
+
+if [ $# -eq 2 ]; then
+    if [[ $1 == "all" ]] || [[ $1 == "auth" ]] || [[ $1 == "world" ]]; then
+        if [[ $2 == "setup" ]] || [[ $2 == "update" ]]; then
+            echo "$1 $2"
+        elif [[ $2 == "database" ]] || [[ $2 == "db" ]]; then
+            echo "$1 $2"
+        elif [[ $2 == "configuration" ]] || [[ $2 == "config" ]] || [[ $2 == "cfg" ]]; then
+            echo "$1 $2"
+        elif [ $2 == "start" ]; then
+            echo "$1 $2"
+        elif [ $2 == "stop" ]; then
+            echo "$1 $2"
+        else
+            invalid_arguments
+        fi
+    else
+        invalid_arguments
+    fi
+else
+    invalid_arguments
+fi
--- includes/functions.sh
+++ includes/functions.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+function invalid_arguments
+{
+    echo -e "\e[0;33mInvalid arguments\e[0m"
+}

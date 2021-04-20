5c23188bfa95f740b2803b94121699d41c55a8ce
Revision <tkn963@gmail.com>
2021-04-20 21:17:53 +0200

    Quest loot
    Fixed several issues I encountered with flags not being set on all quest items. The reason is that not all quest items are 0, so this should fix that issue. It will still not fix items that you loot in order to combine into a quest item but those will be added when found.
    
    This function should only be called when the database is newly imported, so the +2048 shouldn't cause issues.

--- custom/world/questloot.sql
+++ custom/world/questloot.sql
@@ -8,14 +8,13 @@ WHERE Flags = 0 AND entry = 21808;
 IF @FLAG = 0 THEN LEAVE proc; END IF;
 START TRANSACTION;
 
-UPDATE `item_template` SET `Flags`=2048 WHERE `entry` IN (SELECT `RequiredItemId1` FROM `quest_template` WHERE `RequiredItemId1` IN (SELECT `item` FROM `creature_loot_template`)) AND `Flags`=0 AND `class`=12;
-UPDATE `item_template` SET `Flags`=2048 WHERE `entry` IN (SELECT `RequiredItemId2` FROM `quest_template` WHERE `RequiredItemId2` IN (SELECT `item` FROM `creature_loot_template`)) AND `Flags`=0 AND `class`=12;
-UPDATE `item_template` SET `Flags`=2048 WHERE `entry` IN (SELECT `RequiredItemId3` FROM `quest_template` WHERE `RequiredItemId3` IN (SELECT `item` FROM `creature_loot_template`)) AND `Flags`=0 AND `class`=12;
-UPDATE `item_template` SET `Flags`=2048 WHERE `entry` IN (SELECT `RequiredItemId4` FROM `quest_template` WHERE `RequiredItemId4` IN (SELECT `item` FROM `creature_loot_template`)) AND `Flags`=0 AND `class`=12;
-UPDATE `item_template` SET `Flags`=2048 WHERE `entry` IN (SELECT `RequiredItemId5` FROM `quest_template` WHERE `RequiredItemId5` IN (SELECT `item` FROM `creature_loot_template`)) AND `Flags`=0 AND `class`=12;
-UPDATE `item_template` SET `Flags`=2048 WHERE `entry` IN (SELECT `RequiredItemId6` FROM `quest_template` WHERE `RequiredItemId6` IN (SELECT `item` FROM `creature_loot_template`)) AND `Flags`=0 AND `class`=12;
-UPDATE `item_template` SET `Flags`=2048 WHERE `entry` IN (33778, 33779, 33780); -- Book of Runes Chapter 1, 2, 3
-UPDATE `item_template` SET `Flags`=67584 WHERE `entry` IN (37121); -- Ectoplasmic Residue
+UPDATE `item_template` SET `Flags`=Flags+2048 WHERE `entry` IN (SELECT `RequiredItemId1` FROM `quest_template` WHERE `RequiredItemId1` IN (SELECT `item` FROM `creature_loot_template`)) AND `class`=12;
+UPDATE `item_template` SET `Flags`=Flags+2048 WHERE `entry` IN (SELECT `RequiredItemId2` FROM `quest_template` WHERE `RequiredItemId2` IN (SELECT `item` FROM `creature_loot_template`)) AND `class`=12;
+UPDATE `item_template` SET `Flags`=Flags+2048 WHERE `entry` IN (SELECT `RequiredItemId3` FROM `quest_template` WHERE `RequiredItemId3` IN (SELECT `item` FROM `creature_loot_template`)) AND `class`=12;
+UPDATE `item_template` SET `Flags`=Flags+2048 WHERE `entry` IN (SELECT `RequiredItemId4` FROM `quest_template` WHERE `RequiredItemId4` IN (SELECT `item` FROM `creature_loot_template`)) AND `class`=12;
+UPDATE `item_template` SET `Flags`=Flags+2048 WHERE `entry` IN (SELECT `RequiredItemId5` FROM `quest_template` WHERE `RequiredItemId5` IN (SELECT `item` FROM `creature_loot_template`)) AND `class`=12;
+UPDATE `item_template` SET `Flags`=Flags+2048 WHERE `entry` IN (SELECT `RequiredItemId6` FROM `quest_template` WHERE `RequiredItemId6` IN (SELECT `item` FROM `creature_loot_template`)) AND `class`=12;
+UPDATE `item_template` SET `Flags`=Flags+2048 WHERE `entry` IN (33778, 33779, 33780); -- Book of Runes Chapter 1, 2, 3
 
 COMMIT;
 END //

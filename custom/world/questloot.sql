427af01fedff389f52188b5602156870ddcf84f0
Revision <tkn963@gmail.com>
2021-04-20 18:40:47 +0200

    Update questloot.sql
    Changed some phrasing.

--- custom/world/questloot.sql
+++ custom/world/questloot.sql
@@ -2,10 +2,10 @@ DROP PROCEDURE IF EXISTS `questLoot`;
 DELIMITER //
 CREATE PROCEDURE questLoot ()
 proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
-SELECT COUNT(*) INTO @FLAGEXISTS
+SELECT COUNT(*) INTO @FLAG
 FROM item_template
-WHERE Flags = 0 AND entry=21808;
-IF @FLAGEXISTS = 0 THEN LEAVE proc; END IF;
+WHERE Flags = 0 AND entry = 21808;
+IF @FLAG = 0 THEN LEAVE proc; END IF;
 START TRANSACTION;
 
 UPDATE `item_template` SET `Flags`=2048 WHERE `entry` IN (SELECT `RequiredItemId1` FROM `quest_template` WHERE `RequiredItemId1` IN (SELECT `item` FROM `creature_loot_template`)) AND `Flags`=0 AND `class`=12;

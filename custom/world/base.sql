2b7091feea4330ecc51296b005c9c0ba108e7297
Revision <tkn963@gmail.com>
2021-05-02 22:13:43 +0200

    Rework
    Removed quest-obtained spells from the trainers.
    Shamans no longer start with all totems.
    Players no longer start with 36-slot bags.
    Horde now start in Orgrimmar.
    Alliance now start in Stormwind.
    The heirloom vendors are moved to both Orgrimmar and Stormwind.
    The Kickstarter NPC is moved to both Orgrimmar and Stormwind.

--- custom/world/base.sql
+++ custom/world/base.sql
@@ -1,200 +1,36 @@
--- Set spawn point to Dalaran
-UPDATE `playercreateinfo` SET `map`='571', `zone`='4395', `position_x`='5807.810059', `position_y`='587.976990', `position_z`='660.939026', `orientation`='1.663000' WHERE `class` NOT LIKE 6;
+-- Set spawn to Orgrimmar for horde
+UPDATE `playercreateinfo` SET `map`=1, `zone`=1637, `position_x`=1632.663452, `position_y`=-4413.136719, `position_z`=17.030386, `orientation`=0.007035 WHERE `race` IN (2, 5, 6, 8, 10) AND `class` NOT LIKE 6;
+-- Set spawn to Stormwind for alliance
+UPDATE `playercreateinfo` SET `map`=0, `zone`=1519, `position_x`=-8831.482422, `position_y`=624.786682, `position_z`=93.901566, `orientation`=0.794381 WHERE `race` IN (1, 3, 4, 7, 11) AND `class` NOT LIKE 6;
 
--- Foror's Crate of Endless Resist Gear Storage
-DELETE FROM `playercreateinfo_item` WHERE `itemid`='23162';
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (1, 1, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (1, 2, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (1, 4, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (1, 5, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (1, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (1, 8, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (1, 9, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 1, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 3, 23162, 3, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 4, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 7, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 9, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (3, 1, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (3, 2, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (3, 3, 23162, 3, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (3, 4, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (3, 5, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (3, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (4, 1, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (4, 3, 23162, 3, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (4, 4, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (4, 5, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (4, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (4, 11, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (5, 1, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (5, 4, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (5, 5, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (5, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (5, 8, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (5, 9, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (6, 1, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (6, 3, 23162, 3, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (6, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (6, 7, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (6, 11, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (7, 1, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (7, 4, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (7, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (7, 8, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (7, 9, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 1, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 3, 23162, 3, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 4, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 5, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 7, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 8, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (10, 2, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (10, 3, 23162, 3, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (10, 4, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (10, 5, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (10, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (10, 8, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (10, 9, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 1, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 2, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 3, 23162, 3, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 5, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 6, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 7, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 8, 23162, 4, 'Foror\'s Crate of Endless Resist Gear Storage');
-
--- Shaman
--- Earth Totem
-DELETE FROM `playercreateinfo_item` WHERE `itemid`='5175';
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 7, 5175, 1, 'Earth Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (6, 7, 5175, 1, 'Earth Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 7, 5175, 1, 'Earth Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 7, 5175, 1, 'Earth Totem');
--- Fire Totem
-DELETE FROM `playercreateinfo_item` WHERE `itemid`='5176';
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 7, 5176, 1, 'Fire Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (6, 7, 5176, 1, 'Fire Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 7, 5176, 1, 'Fire Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 7, 5176, 1, 'Fire Totem');
--- Water Totem
-DELETE FROM `playercreateinfo_item` WHERE `itemid`='5177';
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 7, 5177, 1, 'Water Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (6, 7, 5177, 1, 'Water Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 7, 5177, 1, 'Water Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 7, 5177, 1, 'Water Totem');
--- Air Totem
-DELETE FROM `playercreateinfo_item` WHERE `itemid`='5178';
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (2, 7, 5178, 1, 'Air Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (6, 7, 5178, 1, 'Air Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (8, 7, 5178, 1, 'Air Totem');
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (11, 7, 5178, 1, 'Air Totem');
-
+-- Set class spell prices to 0
 -- Warrior
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200001, 200002);
--- Defensive Stance
-DELETE FROM `npc_trainer` WHERE `ID`=200002 AND `SpellID`=71;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200002, 71, 0, 0, 0, 10);
--- Taunt
-DELETE FROM `npc_trainer` WHERE `ID`=200002 AND `SpellID`=355;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200002, 355, 0, 0, 0, 10);
--- Sunder Armor
-DELETE FROM `npc_trainer` WHERE `ID`=200002 AND `SpellID`=7386;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200002, 7386, 0, 0, 0, 10);
--- Berserker Stance
-DELETE FROM `npc_trainer` WHERE `ID`=200002 AND `SpellID`=2458;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200002, 2458, 0, 0, 0, 30);
--- Intercept
-DELETE FROM `npc_trainer` WHERE `ID`=200002 AND `SpellID`=20252;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200002, 20252, 0, 0, 0, 30);
-
 -- Paladin
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200003, 200004, 200020, 200021);
--- Redemption
-DELETE FROM `npc_trainer` WHERE `ID`=200003 AND `SpellID`=7328;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200003, 7328, 0, 0, 0, 12);
-DELETE FROM `npc_trainer` WHERE `ID`=200004 AND `SpellID`=7328;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200004, 7328, 0, 0, 0, 12);
-
 -- Hunter
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200013, 200014);
--- Tame Beast
-DELETE FROM `npc_trainer` WHERE `ID`=200014 AND `SpellID`=1515;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200014, 1515, 0, 0, 0, 10);
--- Call Pet
-DELETE FROM `npc_trainer` WHERE `ID`=200014 AND `SpellID`=883;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200014, 883, 0, 0, 0, 10);
--- Dismiss Pet
-DELETE FROM `npc_trainer` WHERE `ID`=200014 AND `SpellID`=2641;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200014, 2641, 0, 0, 0, 10);
--- Revive Pet
-DELETE FROM `npc_trainer` WHERE `ID`=200014 AND `SpellID`=982;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200014, 982, 0, 0, 0, 10);
--- Feed Pet
-DELETE FROM `npc_trainer` WHERE `ID`=200014 AND `SpellID`=6991;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200014, 6991, 0, 0, 0, 10);
-
 -- Rogue
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200015, 200016);
-
 -- Priest
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200011, 200012);
-
 -- Death Knight
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200019);
-
 -- Shaman
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200017, 200018);
--- Stoneskin Totem
-DELETE FROM `npc_trainer` WHERE `ID`=200018 AND `SpellID`=8071;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200018, 8071, 0, 0, 0, 4);
--- Searing Totem
-DELETE FROM `npc_trainer` WHERE `ID`=200018 AND `SpellID`=3599;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200018, 3599, 0, 0, 0, 10);
--- Healing Stream Totem
-DELETE FROM `npc_trainer` WHERE `ID`=200018 AND `SpellID`=5394;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200018, 5394, 0, 0, 0, 20);
-
 -- Mage
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200007, 200008);
-
 -- Warlock
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200009, 200010);
--- Summon Voidwalker
-DELETE FROM `npc_trainer` WHERE `ID`=200010 AND `SpellID`=697;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200010, 697, 0, 0, 0, 10);
--- Summon Succubus
-DELETE FROM `npc_trainer` WHERE `ID`=200010 AND `SpellID`=712;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200010, 712, 0, 0, 0, 20);
--- Summon Felhunter
-DELETE FROM `npc_trainer` WHERE `ID`=200010 AND `SpellID`=691;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200010, 691, 0, 0, 0, 30);
-
 -- Druid
--- Set prices to zero
 UPDATE `npc_trainer` SET `MoneyCost`=0 WHERE `ID` IN (200005, 200006);
--- Bear Form
-DELETE FROM `npc_trainer` WHERE `ID`=200006 AND `SpellID`=5487;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200006, 5487, 0, 0, 0, 10);
--- Growl
-DELETE FROM `npc_trainer` WHERE `ID`=200006 AND `SpellID`=6795;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200006, 6795, 0, 0, 0, 10);
--- Maul
-DELETE FROM `npc_trainer` WHERE `ID`=200006 AND `SpellID`=6807;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES (200006, 6807, 0, 0, 0, 10);
 
--- Kickstarter
-DELETE FROM `creature` WHERE `id`=9000000;
+-- Spawn Kickstarter
+-- Horde
+DELETE FROM `creature` WHERE `guid`=9000000;
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES
-(9000000, 9000000, 571, 4395, 4739, 5811.024414, 625.651367, 647.411682, 1.843417);
+(9000000, 9000000, 1, 1637, 1637, 1651.787964, -4428.298828, 17.071730, 1.782035);
+-- Alliance
+DELETE FROM `creature` WHERE `guid`=9000001;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES
+(9000001, 9000000, 0, 1519, 1519, -8827.855469, 647.297913, 94.538979, 5.294718);
--- custom/world/heirloom.sql
+++ custom/world/heirloom.sql
@@ -5,9 +5,14 @@ DELETE FROM `creature_template` WHERE `entry`=9000010;
 INSERT INTO `creature_template` (`entry`, `modelid1`, `name`, `subname`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `unit_class`) VALUES
 (9000010, 15322, 'Cenarion Emissary Blackhoof', 'Heirloom Armor', 1, 1, 35, 128, 0, 0, 0, 0, 1);
 
-DELETE FROM `creature` WHERE `id`=9000010;
+-- Horde
+DELETE FROM `creature` WHERE `guid`=9000010;
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES
-(9000010, 9000010, 571, 4395, 4395, 5809.225586, 622.384949, 647.759766, 2.174072);
+(9000010, 9000010, 1, 1637, 1637, 1648.981079, -4428.861816, 16.934336, 1.781248);
+-- Alliance
+DELETE FROM `creature` WHERE `guid`=9000011;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES
+(9000011, 9000010, 0, 1519, 1519, -8830.326172, 646.032043, 94.636482, 5.279008);
 
 DELETE FROM `npc_vendor` WHERE `entry`=9000010 AND `item` IN (42943, 42944, 42945, 42946, 42947, 42948, 44091, 44092, 44093, 44094, 44095, 44096, 48716, 48718);
 INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000010, 42943);
@@ -32,40 +37,45 @@ INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000010, 49177);
 -- ----------------------------------------------------------
 -- Heirloom Vendor - Weapons
 -- ----------------------------------------------------------
-DELETE FROM `creature_template` WHERE `entry`=9000011;
+DELETE FROM `creature_template` WHERE `entry`=9000012;
 INSERT INTO `creature_template` (`entry`, `modelid1`, `name`, `subname`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `unit_class`) VALUES
-(9000011, 18648, 'Emissary Mordin', 'Heirloom Weapons', 1, 1, 35, 128, 0, 0, 0, 0, 1);
+(9000012, 18648, 'Emissary Mordin', 'Heirloom Weapons', 1, 1, 35, 128, 0, 0, 0, 0, 1);
 
-DELETE FROM `creature` WHERE `id`=9000011;
+-- Horde
+DELETE FROM `creature` WHERE `guid`=9000012;
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES
-(9000011, 9000011, 571, 4395, 4395, 5799.781738, 621.482483, 647.510315, 1.333695);
+(9000012, 9000012, 1, 1637, 1637, 1654.541260, -4428.017090, 17.193890, 1.773394);
+-- Alliance
+DELETE FROM `creature` WHERE `guid`=9000013;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES
+(9000013, 9000012, 0, 1519, 1519, -8825.722656, 648.786621, 94.543732, 5.251523);
 
-DELETE FROM `npc_vendor` WHERE `entry`=9000011 AND `item` IN (42949, 42950, 42951, 42952, 42984, 42985, 44099, 44100, 44101, 44102, 44103, 44105, 44107, 48677, 48683, 48685, 48687, 48689, 48691, 50255, 42991, 42992, 44097, 44098);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 42949);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 42950);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 42951);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 42952);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 42984);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 42985);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 44099);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 44100);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 44101);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 44102);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 44103);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 44105);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 44107);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 48677);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 48683);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 48685);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 48687);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 48689);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 48691);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 50255);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 42991);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 42992);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 44097);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 44098);
+DELETE FROM `npc_vendor` WHERE `entry`=9000012 AND `item` IN (42949, 42950, 42951, 42952, 42984, 42985, 44099, 44100, 44101, 44102, 44103, 44105, 44107, 48677, 48683, 48685, 48687, 48689, 48691, 50255, 42991, 42992, 44097, 44098);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 42949);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 42950);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 42951);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 42952);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 42984);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 42985);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 44099);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 44100);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 44101);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 44102);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 44103);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 44105);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 44107);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 48677);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 48683);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 48685);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 48687);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 48689);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 48691);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 50255);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 42991);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 42992);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 44097);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 44098);
 
-DELETE FROM `npc_vendor` WHERE `entry`=9000011 AND `item` IN (49177);
-INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000011, 49177);
+DELETE FROM `npc_vendor` WHERE `entry`=9000012 AND `item` IN (49177);
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (9000012, 49177);
 -- ----------------------------------------------------------

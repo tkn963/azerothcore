668ce8b7ae0b7024b7eb9a0d88da04bf171aa7fe
Revision <tkn963@gmail.com>
2021-05-18 20:47:00 +0200

    MariaDB
    Switching to MariaDB from MySQL. Reworked the quest loot to aid in this switch. The previous procedure took so long I almost died from boredom, but this is near instant.

--- includes/packages.sh
+++ includes/packages.sh
@@ -13,7 +13,7 @@ fi
 
 function install_build_packages()
 {
-    PACKAGES=("git" "make" "gcc" "clang" "screen" "curl" "unzip" "g++" "libssl-dev" "libbz2-dev" "libreadline-dev" "libncurses-dev" "libace-6.*" "libace-dev" "cmake" "libmysqlclient-dev" "mysql-client")
+    PACKAGES=("git" "make" "gcc" "clang" "screen" "curl" "unzip" "g++" "libssl-dev" "libbz2-dev" "libreadline-dev" "libncurses-dev" "libace-6.*" "libace-dev" "cmake" "libmariadbclient-dev" "libmariadb-dev-compat" "mariadb-client")
 
     for p in "${PACKAGES[@]}"; do
         if [ $(dpkg-query -W -f='${Status}' $p 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
@@ -36,13 +36,13 @@ function install_build_packages()
 
 function install_database_packages()
 {
-    if [ $(dpkg-query -W -f='${Status}' mysql-client 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    if [ $(dpkg-query -W -f='${Status}' mariadb-client 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
         apt-get update -y
         if [ $? -ne 0 ]; then
             exit 1
         fi
 
-        apt-get install -y mysql-client
+        apt-get install -y mariadb-client
         if [ $? -ne 0 ]; then
             exit 1
         fi

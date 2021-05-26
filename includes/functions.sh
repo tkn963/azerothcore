be22b323b6ec49d7516305422629f79252590c35
Revision <tkn963@gmail.com>
2021-05-26 22:01:17 +0200

    Pull request
    Fixed an issue with using pull requests where it complained about the PR already being active

--- includes/functions.sh
+++ includes/functions.sh
@@ -47,14 +47,17 @@ function build_server()
         fi
     elif [ "$PULL_REQUEST" != "none" ]; then
         cd $CORE_DIRECTORY
-        git checkout -b pr-$PULL_REQUEST
-        if [ $? -ne 0 ]; then
-            exit 1
-        fi
 
-        git pull origin pull/$PULL_REQUEST/head
-        if [ $? -ne 0 ]; then
-            exit 1
+        if [ $(git branch --show-current) != "pr-$PULL_REQUEST" ]; then
+            git checkout -b pr-$PULL_REQUEST
+            if [ $? -ne 0 ]; then
+                exit 1
+            fi
+
+            git pull origin pull/$PULL_REQUEST/head
+            if [ $? -ne 0 ]; then
+                exit 1
+            fi
         fi
     fi
 

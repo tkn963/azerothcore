5b9b0a3513ecfd0c38094db7bedda0a10dcef63a
Revision <tkn963@gmail.com>
2021-05-28 03:24:55 +0200

    Aurora
    Added menus to the assistant. The features are still a work in progress but now there is a base to build off of.

--- lua/aurora.lua
+++ lua/aurora.lua
@@ -66,6 +66,7 @@ local INT_GLYPHS                      = 100
 local INT_GEMS                        = 200
 local INT_HEIRLOOMS                   = 300
 local INT_UTILITIES                   = 400
+local INT_RETURN                      = 1000
 
 -- When a character enters the world
 function onLogin(event, player)
@@ -158,10 +159,10 @@ RegisterPlayerEvent(EVENT_ON_COMMAND, onCommand)
 -- Gossip (Hello)
 function onGossipHello(event, player, object)
     player:GossipClearMenu()
-    player:GossipMenuAddItem(0, "I want glyphs", 1, INT_GLYPHS)
-    player:GossipMenuAddItem(0, "I want gems", 1, INT_GEMS)
-    player:GossipMenuAddItem(0, "I want heirlooms", 1, INT_HEIRLOOMS)
-    player:GossipMenuAddItem(0, "What utilities can I get?", 1, INT_UTILITIES)
+    player:GossipMenuAddItem(7, "I want glyphs", 1, INT_GLYPHS)
+    player:GossipMenuAddItem(7, "I want gems", 1, INT_GEMS)
+    player:GossipMenuAddItem(7, "I want heirlooms", 1, INT_HEIRLOOMS)
+    player:GossipMenuAddItem(7, "What utilities can I get?", 1, INT_UTILITIES)
     player:GossipSendMenu(0x7FFFFFFF, object, 1)
 end
 
@@ -169,23 +170,38 @@ RegisterPlayerGossipEvent(1, 1, onGossipHello)
 
 -- Gossip (Select)
 function onGossipSelect(event, player, object, sender, intid, code)
-    if (intid == INT_HEIRLOOMS) then
+    if (intid == INT_RETURN) then
+        onGossipHello(event, player, player)
+    elseif (intid == INT_GLYPHS) then
         player:GossipClearMenu()
-        player:GossipMenuAddItem(1, "I want some armor", 1, INT_HEIRLOOMS+1, false, "", 0)
-        player:GossipMenuAddItem(1, "I want some weapons", 1, INT_HEIRLOOMS+2, false, "", 0)
+        player:GossipMenuAddItem(7, "I want some major glyphs", 1, INT_GLYPHS+1, false, "", 0)
+        player:GossipMenuAddItem(7, "I want some minor glyphs", 1, INT_GLYPHS+2, false, "", 0)
+        player:GossipMenuAddItem(0, "Return to previous page", 1, INT_RETURN, false, "", 0)
         player:GossipSendMenu(0x7FFFFFFF, object, 1)
-    elseif (intid == INT_HEIRLOOMS+1) then
-        player:SendBroadcastMessage("This feature is unfortunately not yet implemented.")
-        player:GossipComplete()
-    elseif (intid == INT_HEIRLOOMS+2) then
-        player:SendBroadcastMessage("This feature is unfortunately not yet implemented.")
-        player:GossipComplete()
+    elseif (intid == INT_GEMS) then
+        player:GossipClearMenu()
+        player:GossipMenuAddItem(7, "I want some meta gems", 1, INT_GEMS+1, false, "", 0)
+        player:GossipMenuAddItem(7, "I want some red gems", 1, INT_GEMS+2, false, "", 0)
+        player:GossipMenuAddItem(7, "I want some blue gems", 1, INT_GEMS+3, false, "", 0)
+        player:GossipMenuAddItem(7, "I want some yellow gems", 1, INT_GEMS+4, false, "", 0)
+        player:GossipMenuAddItem(7, "I want some purple gems", 1, INT_GEMS+5, false, "", 0)
+        player:GossipMenuAddItem(7, "I want some green gems", 1, INT_GEMS+6, false, "", 0)
+        player:GossipMenuAddItem(7, "I want some orange gems", 1, INT_GEMS+7, false, "", 0)
+        player:GossipMenuAddItem(0, "Return to previous page", 1, INT_RETURN, false, "", 0)
+        player:GossipSendMenu(0x7FFFFFFF, object, 1)
+    elseif (intid == INT_HEIRLOOMS) then
+        player:GossipClearMenu()
+        player:GossipMenuAddItem(7, "I want some armor", 1, INT_HEIRLOOMS+1, false, "", 0)
+        player:GossipMenuAddItem(7, "I want some weapons", 1, INT_HEIRLOOMS+2, false, "", 0)
+        player:GossipMenuAddItem(0, "Return to previous page", 1, INT_RETURN, false, "", 0)
+        player:GossipSendMenu(0x7FFFFFFF, object, 1)
     elseif (intid == INT_UTILITIES) then
         player:GossipClearMenu()
         player:GossipMenuAddItem(6, "I want to change my name", 1, INT_UTILITIES+1, false, "Do you wish to continue the transaction?", UTILITIES_COPPER_RENAME)
         player:GossipMenuAddItem(6, "I want to change my appearance", 1, INT_UTILITIES+2, false, "Do you wish to continue the transaction?", UTILITIES_COPPER_CUSTOMIZE)
         player:GossipMenuAddItem(6, "I want to change my faction", 1, INT_UTILITIES+3, false, "Do you wish to continue the transaction?", UTILITIES_COPPER_CHANGE_FACTION)
         player:GossipMenuAddItem(6, "I want to change my race", 1, INT_UTILITIES+4, false, "Do you wish to continue the transaction?", UTILITIES_COPPER_CHANGE_RACE)
+        player:GossipMenuAddItem(0, "Return to previous page", 1, INT_RETURN, false, "", 0)
         player:GossipSendMenu(0x7FFFFFFF, object, 1)
     elseif (intid == INT_UTILITIES+1) then
         if (player:HasAtLoginFlag(AT_LOGIN_RENAME) or player:HasAtLoginFlag(AT_LOGIN_CUSTOMIZE) or player:HasAtLoginFlag(AT_LOGIN_CHANGE_FACTION) or player:HasAtLoginFlag(AT_LOGIN_CHANGE_RACE)) then
